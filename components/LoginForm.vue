da7bbef5428319411116a5ea0be8aaa611329822
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-08-28 19:42:23 +0800

    Vuetify Form Validation

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -1,11 +1,17 @@
 <template>
   <v-container>
     <v-card>
-      <v-form>
+      <v-form ref="form" v-model="valid" @submit.prevent="onSubmitForm">
         <v-container>
-          <v-text-field label="Email" type="email" required />
-          <v-text-field label="Password" type="password" required />
-          <v-btn color="green" type="submit">Log In</v-btn>
+          <v-text-field v-model="email" :rules="emailRules" label="Email" type="email" required />
+          <v-text-field
+            v-model="password"
+            :rules="passwordlRules"
+            label="Password"
+            type="password"
+            required
+          />
+          <v-btn color="green" type="submit" :disabled="!valid">Log In</v-btn>
           <v-btn nuxt to="/signup">Sign Up</v-btn>
         </v-container>
       </v-form>
@@ -14,7 +20,25 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      valid: false,
+      email: "",
+      password: "",
+      emailRules: [
+        v => !!v || "Email is Required",
+        v => /.+@.+/.test(v) || "Email is Not Valid"
+      ],
+      passwordRules: [v => !!v || "Password is Required"]
+    };
+  },
+  methods: {
+    onSubmitForm() {
+      this.$refs.form.validate();
+    }
+  }
+};
 </script>
 
 <style>
--- pages/signup.vue
+++ pages/signup.vue
@@ -3,12 +3,35 @@
     <v-card>
       <v-container>
         <v-subheader>Sign Up</v-subheader>
-        <v-form>
-          <v-text-field label="Email" type="email" required />
-          <v-text-field label="Password" type="password" required />
-          <v-text-field label="Password Confirm" type="password" required />
-          <v-text-field label="Nickname" type="text" required />
-          <v-checkbox label="Agree?" required />
+        <v-form ref="form" v-model="valid" @submit.prevent="onSubmitForm">
+          <v-text-field v-model="email" label="Email" type="email" :rules="emailRules" required />
+          <v-text-field
+            v-model="password"
+            label="Password"
+            type="password"
+            :rules="passwordRules"
+            required
+          />
+          <v-text-field
+            v-model="passwordConfirm"
+            label="Password Confirm"
+            type="password"
+            :rules="passwordConfirmRules"
+            required
+          />
+          <v-text-field
+            v-model="nickName"
+            label="Nickname"
+            type="text"
+            :rules="nickNameRules"
+            required
+          />
+          <v-checkbox
+            v-model="terms"
+            label="Agree?"
+            :rules="[v => !!v || 'You must agree']"
+            required
+          />
           <v-btn color="green" type="submit">Sign Up</v-btn>
         </v-form>
       </v-container>
@@ -20,7 +43,32 @@
 export default {
   data() {
     return {
-      name: "Nuxt.js"
+      valid: false,
+      email: "",
+      password: "",
+      passwordConfirm: "",
+      nickName: "",
+      terms: false,
+      emailRules: [
+        v => !!v || "Email is Required",
+        v => /.+@.+/.test(v) || "Email is Not Valid"
+      ],
+      nickNameRules: [v => !!v || "NickName is Required"],
+      passwordRules: [v => !!v || "Password is Required"],
+      passwordConfirmRules: [
+        v => !!v || "PasswordConfirm is Required",
+        v => v === this.password || "Password does not match"
+      ]
+    };
+  },
+  methods: {
+    onSubmitForm() {
+      this.$refs.form.validate();
+    }
+  },
+  head() {
+    return {
+      title: "Sign Up"
     };
   }
 };
